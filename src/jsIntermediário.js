e3147af03cf2d1cbd87561b5a26534c39973c165
Flavia Oliveira <flaviacoliv@hotmail.com>
2024-09-30 04:24:51 -0300

    jsInter 300924 2

---
+++ src/jsIntermediário.js
@@ -0,0 +1,11 @@
+const mensagem = 'Olá'
+console.log(mensagem)
+
+// console.log(pais2)
+
+// var pais //declaração
+// pais = 'Brasil' //atribuição
+
+// var pais2 = 'China' //duas instruções separadas, mas, na mesma linha
+// console.log(pais2)
+
