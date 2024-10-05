ccc3a1ab3f8a8de09cdb822a50e85aa68da88435
Flavia Oliveira <flaviacoliv@hotmail.com>
2024-10-04 22:12:50 -0300

    jsInter 041024 1

--- src/jsIntermediário.js
+++ src/jsIntermediário.js
@@ -9,3 +9,16 @@ console.log(mensagem)
 // var pais2 = 'China' //duas instruções separadas, mas, na mesma linha
 // console.log(pais2)
 
+// function somar(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = somar(12, 5)
+
+// console.log(resultadoDaSoma)
+
+let resultadoDaSoma = function(numero1, numero2){
+    return numero1 + numero2
+}
+
+console.log(resultadoDaSoma(12, 5))
