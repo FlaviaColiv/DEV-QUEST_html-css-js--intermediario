7c70e144de918bbbbf573ce69faed884f38ada48
Flavia Oliveira <flaviacoliv@hotmail.com>
2024-10-04 22:18:10 -0300

    jsInter 041024 2

--- src/jsIntermediário.js
+++ src/jsIntermediário.js
@@ -17,8 +17,11 @@ console.log(mensagem)
 
 // console.log(resultadoDaSoma)
 
-let resultadoDaSoma = function(numero1, numero2){
-    return numero1 + numero2
-}
+let resultadoDaSoma = (numero1, numero2) => numero1 + numero2
 
 console.log(resultadoDaSoma(12, 5))
+
+const incentivarQuester = nomeQuester => {
+    console.log('Parabéns ' + nomeQuester + ', Você chegou ao módulo de JS Intermediário!')}
+
+    incentivarQuester('Flávia')
