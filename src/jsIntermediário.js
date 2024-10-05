5ce3abfdb9715d31588040dce8bd52f1bdcfe4b1
Flavia Oliveira <flaviacoliv@hotmail.com>
2024-10-04 22:37:45 -0300

    jsInter 041924 3

--- src/jsIntermediário.js
+++ src/jsIntermediário.js
@@ -25,3 +25,30 @@ const incentivarQuester = nomeQuester => {
     console.log('Parabéns ' + nomeQuester + ', Você chegou ao módulo de JS Intermediário!')}
 
     incentivarQuester('Flávia')
+
+const calcularAnoDeNascimento = function (idade, mesDeNascimento, imprimir){
+    const mesAtual = 10
+    let anoDeNascimento = 2024 - idade
+    if(mesDeNascimento > mesAtual) anoDeNascimento--
+    imprimir(anoDeNascimento)
+}
+
+let imprimirAnoDeNascimento = anoDeNascimento => {
+    console.log('Seu ano de nascimento é: ' + anoDeNascimento)
+}
+
+calcularAnoDeNascimento(35, 7, imprimirAnoDeNascimento)
+
+function multiplicar(multiplicador){
+    return function(numero){
+        return numero * multiplicador
+    }
+}
+
+let dobrar = multiplicar(2)
+let triplicar = multiplicar(3)
+let quadruplicar = multiplicar(4)
+
+console.log(dobrar(3))
+console.log(triplicar(3))
+console.log(quadruplicar(3))
