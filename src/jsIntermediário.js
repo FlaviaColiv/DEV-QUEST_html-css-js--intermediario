8db34d77168c7b8bacf1768dfa2b008c29a31646
Flavia Oliveira <flaviacoliv@hotmail.com>
2024-10-15 17:53:03 -0300

    jsInter 151024 5

--- src/jsIntermediário.js
+++ src/jsIntermediário.js
@@ -257,3 +257,14 @@ function aumentarFonteSegundoPost() {
 
     textoPosts[1].classList.add('fonte-grande')
 }
+
+function marcarRadio(genero) {
+    let radioMasculino = document.getElementById('genero-masculino');
+    let radioFeminino = document.getElementById('genero-feminino');
+
+    if(genero === 'M') {
+        radioMasculino.checked = true;
+    } else if(genero === 'F') {
+        radioFeminino.checked = true;
+    }
+}
